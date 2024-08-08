0cab80a2cc633f4a18c2f00c6786b4c22c5d7864
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2024-08-09 00:42:20 +0700

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "pn-primary": "#004040",
+        "pn-secondary": "#D6B66B",
+        "pn-terniary": "#0B7373",
+        "pn-gray": "#B8B8B8",
+      },
+      fontFamily: {
+        unbounded: "var(--font-unbounded)",
+        "alex-brush": "var(--font-alex-brush)",
+        "albert-sans": "var(--font-albert-sans)",
+      },
     },
   },
   plugins: [],
